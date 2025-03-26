94a418696a0e0b9bb5e88e50083179f6538f6f0b
Owen Dewing <odewing@lion.lmu.edu>
2025-03-26 16:35:33 -0700

    SDD and status report

---
+++ Globetrotter_App/Globetrotter/frontend/app/HomeScreen.tsx
@@ -0,0 +1,17 @@
+import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
+import React, {useState} from 'react';
+
+export default function HomeScreen() {
+    return (
+        <View style = {styles.container}>
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#4C809C"
+    }
+})
